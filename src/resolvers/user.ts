1ed552741a67519d358126d8b7ae30acb6f2b4ee
yasserqamhiah <yaserqamhiah@gmail.com>
2022-07-09 23:08:29 +0300

    Implement Login

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,8 +1,9 @@
 import { RequiredEntityData } from "@mikro-orm/core";
 import argon2  from "argon2";
-import { User } from "src/entities/User";
+import { User } from "../entities/User";
 import { MyContext } from "src/types";
-import { Arg, Ctx, Field, InputType, Mutation, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver } from "type-graphql";
+import { errors } from "puppeteer";
 
 @InputType()
 class UsernamePasswordInput{
@@ -10,6 +11,22 @@ class UsernamePasswordInput{
 username:string
 @Field()
 password:string
+}
+@ObjectType()
+class UserResponse{
+    
+    @Field(()=> [FieldError], {nullable:true})
+    errors?:FieldError[]
+    @Field(()=> User ,{nullable:true})
+    user?:User 
+}
+
+@ObjectType()
+class FieldError{
+    @Field()
+    field:string
+    @Field()
+    message:string
 }
 
 
@@ -29,5 +46,37 @@ async register(
     await em.persistAndFlush(user)
     return user; 
 }
+
+
+@Mutation(()=>UserResponse)
+async login(
+    @Arg("options") options:UsernamePasswordInput,
+    @Ctx(){em}:MyContext
+):Promise<UserResponse>{
+
+
+
+    const user=await em.findOne(User, {username:options.username})
+    if(!user){
+        return {
+            errors:[{field:'username',message:"That username doesn't exist"}]
+        }
+    }
+    
+
+    const vaildPassword=await argon2.verify(user.password,options.password)
+    if(!vaildPassword){
+        return {
+            errors:[{field:'password',message:"Incorrect Password Try again"}]
+        }
+
+    }
+    return {
+        user
+    }
+}
+
+
+
 
 }
