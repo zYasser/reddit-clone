56c4b67701cdc79fb3332d7e861b0ef2106b19b8
yasserqamhiah <yaserqamhiah@gmail.com>
2022-07-14 16:41:19 +0300

    Update register page, added submit button

--- front-end/src/pages/register.tsx
+++ front-end/src/pages/register.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 import { Form, Formik } from "formik";
-import {
-  FormControl,
-  FormLabel,
-  Input,
-  FormErrorMessage,
-} from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputFieldProps";
 
@@ -18,19 +13,24 @@ const Register: React.FC<registerProps> = ({}) => {
         initialValues={{ username: "", passowrd: "" }}
         onSubmit={(values) => console.log(values)}
       >
-        {({ values, handleChange }) => (
+        {({ isSubmitting }) => (
           <Form>
             <InputField
               name="username"
               placeholder="username"
               label="Username"
             ></InputField>
-            <InputField
-              name="password"
-              placeholder="password"
-              label="Password"
-              type="password"
-            ></InputField>
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              ></InputField>
+            </Box>
+            <Button mt={4} type="submit" isLoading={isSubmitting} colorScheme="teal">
+              Register
+            </Button>
           </Form>
         )}
       </Formik>
